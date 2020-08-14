6e0f16356bd82a60a30a91b2f5d4b012a8316663
ChrisBui <chrisbui@Chriss-MacBook-Pro.local>
2020-08-14 15:11:43 -0400

    passport authentication

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,43 +37,29 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'sesson-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error('You are not logged in');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
+  console.log(req.session);
 
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username == 'admin' && password == 'password') {
-      res.cookie('user', 'admin', { signed: true });
-      next();
-    }
-    else {
-      var err = new Error('You are not logged in');
-      res.setHeader('WW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
   else {
-    if (req.signedCookies.user === 'admin') {
-      next();
-    }
-    else {
-      var err = new Error('You are not logged in');
-      err.status = 401;
-      next(err);
-    }
+    next();
   }
 }
 app.use(auth);
@@ -77,8 +67,6 @@ app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/leaders', leaderRouter);
 app.use('/promotions', promotionRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/users');
+const { route } = require('./dishRouter');
+var passport = require('passport');
+
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  User.find({})
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    })
+    .catch(err => next(err));
 });
+
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+    if (err) {
+      res.status = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ err: err });
+    }
+    else {
+      passport.authenticate('local')(req, res, () => {
+        res.status = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ success: true, status: 'Registration Successful' });
+        console.log('lagging');
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.status = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ status: 'You are logged in', success: true });
+});
+
+
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+    res.clearCookie('sesson-id');
+    res.redirect('/');
+  }
+  else {
+    var err = new Error('You are not logged in');
+    next(err);
+  }
+});
+
 
 module.exports = router;
